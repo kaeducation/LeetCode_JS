84a6a94e716e1296be11fdbca1fdb51dc168fc3a
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-04-20 14:30:36 -0700

    Added LC7

---
+++ exercises/LC7_reverse/index.js
@@ -0,0 +1,12 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+// For an int overflow, your code should just return 0.
+// -------------------------
+// reverse(123) --> 321
+// reverse(120) --> 21
+// reverse(-123) --> -321
+// reverse(2147483649) --> 0
+// reverse(-2147483649) --> 0
+
+function reverse(x) {}
+
+module.exports = reverse;
---
+++ exercises/LC7_reverse/test.js
@@ -0,0 +1,21 @@
+const reverse = require("./index");
+
+test("reverse(123) returns 321", () => {
+  expect(reverse(123)).toEqual(321);
+});
+
+test("reverse(120) returns 21", () => {
+  expect(reverse(120)).toEqual(21);
+});
+
+test("reverse(-123) returns -321", () => {
+  expect(reverse(-123)).toEqual(-321);
+});
+
+test("returns 0 on int overflow for positive number", () => {
+  expect(reverse(2147483649)).toEqual(0);
+});
+
+test("returns 0 on int overflow for negative number", () => {
+  expect(reverse(-2147483649) * -1).toEqual(0);
+});
