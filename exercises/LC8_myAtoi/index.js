443b922e9f926dcd0f1e066aae35c0694c2da9a5
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-04-20 16:41:35 -0700

    Added LC8

---
+++ exercises/LC8_myAtoi/index.js
@@ -0,0 +1,16 @@
+//Implement atoi which converts a string to an integer.
+// Your code should pass all of the examples below...
+// -------------------------
+// myAtoi("42") --> 42
+// myAtoi("  0000000000012345678") --> 12345678
+// myAtoi("+1") --> 1
+// myAtoi("   -42") --> -42
+// myAtoi("4193 with words") --> 4193
+// myAtoi("words and 987") --> 0
+// myAtoi("3.14159") --> 3
+// myAtoi("2147483648") --> 2147483647
+// myAtoi("-91283472332") --> -2147483648
+
+function myAtoi(str) {}
+
+module.exports = myAtoi;
--- scratch.js
+++ scratch.js
@@ -1,1 +1,35 @@
-[["A", "E"], ["B", "D", "F", "H"], ["C", "G"]];
+function myAtoi(str) {
+  str = str.trim();
+  const digits = "0123456789";
+  const hashMap = digits.split("").reduce((obj, next, i) => {
+    obj[next] = i;
+    return obj;
+  }, {});
+  let sign = 1;
+
+  if (str[0] === "-") {
+    sign = -1;
+    str = str.slice(1);
+  } else if (str[0] === "+") {
+    str = str.slice(1);
+  } else if (hashMap[str[0]] === undefined) {
+    return 0;
+  }
+
+  let res = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (hashMap[char] !== undefined) {
+      res = res * 10 + hashMap[char];
+    } else {
+      break;
+    }
+  }
+
+  res = res > 2147483648 ? 2147483648 : res;
+  console.log("reached");
+  return sign * res;
+}
+
+myAtoi("  0000000000012345678");
