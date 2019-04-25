4fa351ef3eeea964794a390982387fdcd30a921c
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-04-24 17:34:05 -0700

    Added lc13

---
+++ exercises/LC13_romanToInt/index.js
@@ -0,0 +1,12 @@
+// Given a roman numeral as a string, convert it to an integer. Input is guaranteed
+// to be within the range from 1 to 3999.
+// ---------------------------------------------
+// romanToInt("III") --> 3
+// romanToInt("IV") --> 4
+// romanToInt("IX") --> 9
+// romanToInt("LVIII") --> 58
+// romanToInt("MCMXCIV") --> 1994
+
+const romanToInt = s => {};
+
+module.exports = romanToInt;
--- scratch.js
+++ scratch.js
@@ -1,26 +1,3 @@
-var romanToInt = function(s) {
-  let sum = 0;
-
-  const romanToIntMap = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-  };
-
-  if (s.indexOf("IV")) sum -= 2;
-  if (s.indexOf("IX")) sum -= 2;
-  if (s.indexOf("XL")) sum -= 20;
-  if (s.indexOf("XC")) sum -= 20;
-  if (s.indexOf("CD")) sum -= 200;
-  if (s.indexOf("CM")) sum -= 200;
-
-  s.split("").forEach(char => {
-    sum += romanToIntMap[char];
-  });
-
-  return sum;
-};
+const yo = { c: "meow" };
+console.log("yo.c", yo.c);
+console.log("yo.C", yo.C);
