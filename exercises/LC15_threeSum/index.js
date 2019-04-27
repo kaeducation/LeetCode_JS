718bdb49f2c294c993b62966a1442ee3e953c11a
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-04-27 16:07:44 -0700

    Added LC15

---
+++ exercises/LC15_threeSum/index.js
@@ -0,0 +1,3 @@
+const threeSum = nums => {};
+
+module.exports = threeSum;
--- scratch.js
+++ scratch.js
@@ -1,11 +1,5 @@
-const longestCommonPrefix = function(strs) {
-  let prefix = strs.length ? strs[0] : "";
-
-  strs.forEach(str => {
-    while (str.indexOf(prefix) !== 0) {
-      prefix = prefix.slice(0, -1);
-    }
-  });
-
-  return prefix;
-};
+const strs = ["aacc", "aab", "ab", "a"];
+//              ^
+//              |
+//              |
+let LCP = "aacc";
