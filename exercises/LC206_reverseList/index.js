e7c8057b34a629888c391a8d246a8ba9c41f18ba
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-05-06 17:45:35 -0700

    Removed solution

--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,19 +1,3 @@
-const reverseList = head => {
-  let prev = null;
-  let current = head;
-
-  while (current) {
-    let temp = current.next;
-
-    //Change direction
-    current.next = prev;
-
-    //Move prev and current up by 1
-    prev = current;
-    current = temp;
-  }
-
-  return prev;
-};
+const reverseList = head => {};
 
 module.exports = reverseList;
