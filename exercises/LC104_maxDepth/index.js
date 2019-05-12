2c95717108aecd9539a12eea8ecb067a9bba85f1
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-05-12 16:13:53 -0700

    Added LC104

---
+++ exercises/LC104_maxDepth/index.js
@@ -0,0 +1,3 @@
+const maxDepth = root => {};
+
+module.exports = maxDepth;
--- scratch.js
+++ scratch.js
@@ -1,53 +1,23 @@
-function setZeroes(matrix) {
-  let firstColHasZero = false;
-  let firstRowHasZero = false;
-
-  // Check if first col has zero
-  for (let i = 0; i < matrix.length; i++) {
-    if (matrix[i][0] === 0) {
-      firstColHasZero = true;
-      break;
-    }
+const maxDepth = root => {
+  if (!root) {
+    return 0;
   }
 
-  // Check if first row has zero
-  for (let j = 0; j < matrix[0].length; j++) {
-    if (matrix[0][j] === 0) {
-      firstRowHasZero = true;
-      break;
-    }
-  }
+  const queue = [root];
+  let count = 0;
 
-  // Use first row and col as flags, set matrix[i][0] and matrix[0][j] to 0 if matrix[i][j] is 0
-  for (let i = 1; i < matrix.length; i++) {
-    for (let j = 1; j < matrix[0].length; j++) {
-      if (matrix[i][j] === 0) {
-        matrix[0][j] = 0;
-        matrix[i][0] = 0;
+  while (queue.length > 0) {
+    const size = queue.length;
+    for (let i = size; i > 0; i--) {
+      const cur = queue.pop();
+      if (cur.left) {
+        queue.unshift(cur.left);
+      }
+      if (cur.right) {
+        queue.unshift(cur.right);
       }
     }
+    count++;
   }
-
-  // Zero out cells based on flags in first row and col
-  for (let i = 1; i < matrix.length; i++) {
-    for (let j = 1; j < matrix[0].length; j++) {
-      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
-        matrix[i][j] = 0;
-      }
-    }
-  }
-
-  // Zero out first col
-  if (firstColHasZero) {
-    for (let i = 0; i < matrix.length; i++) {
-      matrix[i][0] = 0;
-    }
-  }
-
-  // Zero out first row
-  if (firstRowHasZero) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      matrix[0][j] = 0;
-    }
-  }
-}
+  return count;
+};
