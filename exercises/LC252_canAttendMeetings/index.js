a4433cdfcb992c5b992adf115289690590b1874f
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-05-14 15:41:51 -0700

    Fixed LC252

--- exercises/LC252_canAttendMeetings/index.js
+++ exercises/LC252_canAttendMeetings/index.js
@@ -4,24 +4,6 @@
 // canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
 // canAttendMeetings([[7, 10], [2, 4]]) --> true
 
-const canAttendMeetings = intervals => {
-  const starts = [];
-  const ends = [];
-
-  for (let i = 0; i < intervals.length; i++) {
-    const subArray = intervals[i];
-    starts.push(subArray[0]);
-    ends.push(subArray[1]);
-  }
-
-  starts.sort((a, b) => a - b);
-  ends.sort((a, b) => a - b);
-
-  for (let i = 0; i < starts.length; i++) {
-    if (starts[i + 1] < ends[i]) return false;
-  }
-
-  return true;
-};
+const canAttendMeetings = intervals => {};
 
 module.exports = canAttendMeetings;
