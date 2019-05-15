0827f1825b3c913df037ec328534859f3a04e7e2
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-05-14 17:48:47 -0700

    Removed sol

--- exercises/LC435_eraseOverlapIntervals/index.js
+++ exercises/LC435_eraseOverlapIntervals/index.js
@@ -1,26 +1,3 @@
-const eraseOverlapIntervals = intervals => {
-  if (intervals.length === 0) return 0;
-
-  let count = 0;
-
-  intervals.sort((a, b) => a[0] - b[0]);
-
-  let end = intervals[0][1];
-
-  for (let i = 1; i < intervals.length; i++) {
-    const interval = intervals[i];
-    const intervalStart = interval[0];
-    const intervalEnd = interval[1];
-
-    if (intervalStart < end) {
-      count++;
-      end = Math.min(intervalEnd, end);
-    } else {
-      end = intervalEnd;
-    }
-  }
-
-  return count;
-};
+const eraseOverlapIntervals = intervals => {};
 
 module.exports = eraseOverlapIntervals;
