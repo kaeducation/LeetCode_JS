dbab2a6240e9fdae0b72fa4bc3e9ed2928baf9ad
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-05-17 15:23:56 -0700

    Removed sol

--- exercises/LC98_isValidBST/index.js
+++ exercises/LC98_isValidBST/index.js
@@ -1,27 +1,3 @@
-const isValidBST = root => {
-  if (!root) {
-    return true;
-  }
-
-  let validity = true;
-
-  const helper = (node, min, max) => {
-    if (!node) return;
-
-    if (
-      (min !== null && node.val <= min) ||
-      (max !== null && node.val >= max)
-    ) {
-      validity = false;
-      return;
-    }
-
-    helper(node.left, min, node.val);
-    helper(node.right, node.val, max);
-  };
-
-  helper(root, null, null);
-  return validity;
-};
+const isValidBST = root => {};
 
 module.exports = isValidBST;
