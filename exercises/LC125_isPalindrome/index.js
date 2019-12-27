be867005fe2edc303d87c39b50a8a21f66f11e3f
Ke Chen <keke@Kes-MacBook-Air.local>
2019-12-27 00:23:15 -0800

    isPalindrome

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,18 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+    let onlyChar = s.toLowerCase().replace(/[\W_]/g, "");
+    console.log(onlyChar);
+    console.log(onlyChar.length - 1)
+
+    for (let x = 0; x < onlyChar.length; x++) {
+        if (onlyChar[x] !== onlyChar[onlyChar.length - 1 - x]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome('love'));
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+console.log(isPalindrome('_a__'));
 
 module.exports = isPalindrome;
